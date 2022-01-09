53af822ecbe23faf9f2f05d3422218bc22568ee0
weiwi <marcoer@gmail.com>
2022-01-09 21:52:41 +0800

    Initial Commit

---
+++ 3rdPartySPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3rdPartySPM
+//
+//  Created by WEI-YI Wang on 2022/1/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
